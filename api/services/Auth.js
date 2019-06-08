88c42fee328a1ad04d7959777c3af00bafe50ee6
BallSurakiat-PC <dosforrentfilm@gmail.com>
2019-06-09 02:16:55 +0700

    [M] add user test (admin, user, guest)

--- api/services/Auth.js
+++ api/services/Auth.js
@@ -8,16 +8,39 @@ const express = require('express')
 const authApp = express.Router()
 
 authApp.post('/client', function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
+    var user
+
     console.log({
         username: req.body.username,
         password: req.body.password
     });
-    res.json({
-        token: 'token',
-        rule: 0,
-        username: 'Admin',
-        id: '1'
-    })
+
+    if (username == 'admin' && password == '1234') {
+        user = {
+            token: 'token',
+            rule: 1,
+            username: 'Admin VIP',
+            id: '1'
+        }
+    } else if (username == 'user' && password == '1234') {
+        user = {
+            token: 'token',
+            rule: 2,
+            username: 'User test',
+            id: '1'
+        }
+    } else {
+        user = {
+            token: 'token',
+            rule: 0,
+            username: 'Guest Test',
+            id: '1'
+        }
+    }
+
+    res.json(user)
 })
 
 module.exports = authApp
